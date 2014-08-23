b48b45bcd5b5570bca7ba31cdc4d06ad50edbb63
Leandro Alemao <leandro.costantini@gmail.com>
2014-08-23 19:54:44 +0100

    more tests and learning

--- test/unit/controllersSpec.js
+++ test/unit/controllersSpec.js
@@ -18,3 +18,27 @@ describe('controllers', function(){
     expect(myCtrl2).toBeDefined();
   }));
 });
+
+/* jasmine specs for controllers go here */
+describe('PhoneCat controllers', function() {
+
+  describe('PhoneListCtrl', function(){
+    var scope, ctrl;
+
+    beforeEach(module('myApp'));
+
+    beforeEach(inject(function($controller) {
+      scope = {};
+      ctrl = $controller('PhoneListCtrl', {$scope:scope});
+    }));
+
+    it('should create "phones" model with 3 phones', function() {
+      expect(scope.phones.length).toBe(3);
+    });
+
+
+    it('should set the default value of orderProp model', function() {
+      expect(scope.orderProp).toBe('age');
+    });
+  });
+});
